ef79fa6ad41ca3876b5e81a9f973e93d7b85300f
Emilio Cobos Álvarez <emilio@crisal.io>
2017-03-16 13:26:50 +0100

    Correctly position fraction children, modulo customisation.

--- third_party/WebKit/Source/core/layout/mathml/ng_mathml_fraction_node.cc
+++ third_party/WebKit/Source/core/layout/mathml/ng_mathml_fraction_node.cc
@@ -4,8 +4,10 @@
 
 #include "core/layout/mathml/ng_mathml_fraction_node.h"
 
+#include "core/layout/ng/ng_box_fragment.h"
 #include "core/layout/ng/ng_constraint_space_builder.h"
 #include "core/layout/ng/ng_fragment_builder.h"
+#include "core/layout/ng/ng_length_utils.h"
 
 namespace blink {
 
@@ -19,22 +21,85 @@ RefPtr<NGLayoutResult> NGMathMLFractionNode::Layout(
   LayoutUnit linethickness =
       toUserUnits(m_linethickness, default_linethickness);
 
-  // TODO: Do the actual fraction layout.
+  NGBlockNode* numerator = toNGBlockNode(FirstChild());
+  NGBlockNode* denominator =
+    numerator ? toNGBlockNode(numerator->NextSibling()) : nullptr;
+
+  // Invalid markup: display nothing. We could create an anonymous
+  // TextLayoutObject or something, even in createLayoutObject instead of here.
+  //
+  // Anyway, this matches WebKit's behavior for now.
   NGFragmentBuilder builder(NGPhysicalFragment::kFragmentBox, this);
-  NGConstraintSpaceBuilder child_constraints(constraint_space);
-  for (NGBlockNode* child = toNGBlockNode(FirstChild()); child;
-       child = toNGBlockNode(child->NextSibling())) {
-    RefPtr<NGConstraintSpace> child_constraint_space =
-        child_constraints.ToConstraintSpace(
-            FromPlatformWritingMode(child->Style().getWritingMode()));
+
+  if (!numerator || !denominator || denominator->NextSibling()) {
     RefPtr<NGLayoutResult> result =
-        child->Layout(child_constraint_space.get());
-    builder.AddChild(std::move(result), NGLogicalOffset());
+      builder.SetBlockSize(LayoutUnit())
+        .SetInlineSize(LayoutUnit())
+        .ToBoxFragment();
+
+    CopyFragmentDataToLayoutBox(*constraint_space, result.get());
+
+    return result;
   }
 
+  // FIXME(emilio): This is kinda boilerplate, right? Just make it shared with
+  // other code.
+  NGLogicalSize available_size = constraint_space->AvailableSize();
+  NGBoxStrut border_padding = ComputeBorders(*constraint_space, Style()) +
+                              ComputePadding(*constraint_space, Style());
+
+  NGLogicalSize adjusted_size(
+      available_size.inline_size - border_padding.InlineSum(),
+      available_size.block_size == NGSizeIndefinite
+        ? NGSizeIndefinite : available_size.block_size - border_padding.BlockSum());
+
+  NGConstraintSpaceBuilder child_constraints(constraint_space);
+  child_constraints.SetAvailableSize(adjusted_size)
+    .SetPercentageResolutionSize(adjusted_size);
+
+  // TODO(emilio): It's not clear what to do if they don't fit in the constraint
+  // space...
+  RefPtr<NGConstraintSpace> numerator_constraint_space =
+      child_constraints.ToConstraintSpace(
+          FromPlatformWritingMode(numerator->Style().getWritingMode()));
+  RefPtr<NGLayoutResult> numerator_layout_result =
+      numerator->Layout(numerator_constraint_space.get());
+
+  RefPtr<NGConstraintSpace> denominator_constraint_space =
+      child_constraints.ToConstraintSpace(
+          FromPlatformWritingMode(denominator->Style().getWritingMode()));
+  RefPtr<NGLayoutResult> denominator_layout_result =
+      denominator->Layout(denominator_constraint_space.get());
+
+  // TODO(emilio): We need to change the horizontal offset depending on the
+  // numalign and denalign attributes. Assume left for now.
+  NGLogicalOffset numerator_offset;
+  NGLogicalOffset denominator_offset;
+
+  NGBoxFragment numerator_fragment(
+      FromPlatformWritingMode(numerator->Style().getWritingMode()),
+      toNGPhysicalBoxFragment(numerator_layout_result->PhysicalFragment().get()));
+  NGBoxFragment denominator_fragment(
+      FromPlatformWritingMode(denominator->Style().getWritingMode()),
+      toNGPhysicalBoxFragment(denominator_layout_result->PhysicalFragment().get()));
+
+  LayoutUnit min_numerator_gap, min_denominator_gap; // TODO: Get from attributes.
+
+  denominator_offset.block_offset =
+    numerator_fragment.BlockSize() + min_numerator_gap + linethickness +
+    min_denominator_gap;
+
+  builder.AddChild(std::move(numerator_layout_result), numerator_offset);
+  builder.AddChild(std::move(denominator_layout_result), denominator_offset);
+
+  LayoutUnit total_block_size =
+    denominator_offset.block_offset + denominator_fragment.BlockSize();
+  LayoutUnit total_inline_size =
+    std::max(numerator_fragment.InlineSize(), denominator_fragment.InlineSize());
+
   RefPtr<NGLayoutResult> result =
-      builder.SetBlockSize(linethickness)
-             .SetInlineSize(LayoutUnit(100))
+      builder.SetBlockSize(total_block_size)
+             .SetInlineSize(total_inline_size)
              .ToBoxFragment();
 
   CopyFragmentDataToLayoutBox(*constraint_space, result.get());
